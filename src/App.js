9b0bbb250eb57c6daefd29a534a4db974dd77429
John Lamis <lamisjohn@gmail.com>
2018-08-10 03:48:13 +0300

    loginform change proposition

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
 
-import Auth from './containers/Auth/Auth'; 
+import Auth from "./containers/Auth/Auth";
 
 import "./App.css";
-
 
 class App extends Component {
   render() {
     return (
       <Switch>
-        <Route path='/login' component={Auth} />
-        <Route path='/' render={null} />
+        <Route path="/login" component={Auth} />
+        <Route path="/" render={null} />
       </Switch>
     );
   }
